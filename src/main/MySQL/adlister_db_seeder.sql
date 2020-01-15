a364c5c68196d89b0675e8a8b37883d98b8bc25d
colinmarchbanks <marchbanks.colin@gmail.com>
2020-01-15 09:24:38 -0600

    Working on the ads mysql connection

---
+++ src/main/MySQL/adlister_db_seeder.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE IF NOT EXISTS adlister_db;
+
+USE adlister_db;
+
+CREATE TABLE IF NOT EXISTS users
+(
+    id       INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    email    VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS ads
+(
+    id       INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id  INT UNSIGNED NOT NULL,
+    title    VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
---
+++ src/main/java/MySQLAdsDao.java
@@ -0,0 +1,46 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySQLAdsDao implements Ads {
+
+    private Connection connection;
+
+    public MySQLAdsDao(Config config) throws SQLException {
+        this.connection = DriverManager.getConnection(config.getUrl(), config.getUsername(), config.getPassword());
+    }
+
+    @Override
+    public List<Ad> all() throws SQLException {
+        Statement statement = connection.createStatement();
+        List<Ad> bucket = new ArrayList<>();
+        ResultSet rs = statement.executeQuery("SELECT * FROM ads");
+
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            int user_id = rs.getInt("user_id");
+            String title = rs.getString("title");
+            String description = rs.getString("description");
+
+
+            Ad ad = new Ad(id, user_id, title, description);
+
+            bucket.add(ad);
+        }
+        return bucket;
+    }
+
+
+    @Override
+    public Long insert(Ad ad) throws SQLException {
+        String query = "INSERT INTO ads(user_id, title, description) VALUES (" + ad.getUserId() + "," + ad.getTitle() + ", " + ad.getDescription() + ")";
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate(query, Statement.RETURN_GENERATED_KEYS);
+        ResultSet rs = stmt.getGeneratedKeys();
+        if (rs.next()) {
+            System.out.println("Inserted a new record! New id: " + rs.getLong(1));
+        }
+        return rs.getLong(1);
+
+    }
+}
